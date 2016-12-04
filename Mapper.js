9fd2c9ba37b69c96087fc2da6d7aded2facd02f6
James B. Pollack <jamesbradenpollack@gmail.com>
2016-12-04 13:21:10 -0800

    updating realtime data

--- Mapper.js
+++ Mapper.js
@@ -5,7 +5,6 @@ function te_Mapper() {
 
 te_Mapper.prototype = {
     refreshRate: 15,
-    liveData: {},
     baseMapNames: [
         'neighborhoods',
         'streets',
@@ -14,6 +13,7 @@ te_Mapper.prototype = {
     ],
     baseMapGeoJSON: [],
     baseMapGroups: [],
+    vehicleGroups: [],
     setupDrawingSpace: function() {
         var width = window.innerWidth,
             height = window.innerHeight;
@@ -35,7 +35,6 @@ te_Mapper.prototype = {
         _t.baseMapNames.forEach(function(mapName) {
             _t.loadBaseMap(mapName)
         })
-
     },
 
     loadBaseMap: function(mapName) {
@@ -56,25 +55,25 @@ te_Mapper.prototype = {
                 console.log('Waiting for basemaps to finish loading')
             }
         });
-
-
     },
+
     drawBaseMaps: function() {
         var _t = this;
         _t.baseMapNames.forEach(function(mapName) {
             var geoJSON = _t.getBaseMapGeoJSONByName(mapName);
-            _t.addBaseMapLayer(geoJSON);
+            _t.addBaseMapLayer(geoJSON, mapName);
         })
     },
+
     getBaseMapGeoJSONByName: function(mapName) {
         return this.baseMapGeoJSON.filter(function(obj) {
             return obj.name == mapName;
         })[0];
     },
 
-    addBaseMapLayer: function(geojson) {
+    addBaseMapLayer: function(geojson, mapName) {
         var _t = this;
-        var svgGroup = _t.svg.append("g");
+        var svgGroup = _t.svg.append("g").attr('id', 'layer_' + mapName);
 
         var geoPath = d3.geoPath()
             .projection(this.projection);
@@ -96,30 +95,211 @@ te_Mapper.prototype = {
         this.baseMapGroups.push(svgGroup);
 
     },
+
     mouseOver: function(val) {
         console.log('val mouseOver', val)
     },
+
     mouseOut: function(val) {
         console.log('val mouseOut', val)
     },
+
     clicked: function(val) {
         console.log('val clicked', val.properties)
     },
-    fetchNextBusXMLFeed: function() {
+
+    fetchRouteList: function() {
+        var _t = this;
+        var routeListURL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni';
+
+        var p = new Promise(function(resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', routeListURL);
+            xhr.send(null);
+            xhr.onerror = reject;
+            xhr.onreadystatechange = function() {
+                var DONE = 4;
+                var OK = 200;
+                if (xhr.readyState === DONE) {
+                    if (xhr.status === OK) {
+                        var parsedRouteList = xmlToJson(xhr.responseXML);
+                        if (parsedRouteList.hasOwnProperty('body') && parsedRouteList.body.hasOwnProperty('Error')) {
+                            reject(parsedRouteList.body.Error)
+                        } else {
+                            resolve(parsedRouteList);
+
+                        }
+                    }
+                }
+            }
+        });
+
+        return p
+    },
+
+    fetchRoute: function(tag) {
+        var _t = this;
+        var tag = tag || '';
+        tag = tag.toUpperCase();
+
+        var routeURL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni&r=' + tag;
+
+        var p = new Promise(function(resolve, reject) {
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', routeURL);
+            xhr.send(null);
+            xhr.onerror = reject;
+
+            xhr.onreadystatechange = function() {
+                var DONE = 4;
+                var OK = 200;
+                if (xhr.readyState === DONE) {
+                    if (xhr.status === OK) {
+                        var parsedRoute = xmlToJson(xhr.responseXML);
+                        if (parsedRoute.hasOwnProperty('body') && parsedRoute.body.hasOwnProperty('Error')) {
+                            reject(parsedRoute.body.Error)
+                        } else {
+                            resolve(parsedRoute);
+                        }
+                    }
+                }
+            }
+        });
+
+        return p
 
     },
-    parseNextBusXMLFeed: function() {
+
+    fetchVehicleLocations: function(tag, epochTime) {
+        var _t = this;
+        var tag = tag || '5';
+        tag = tag.toUpperCase();
+        var epochTime = epochTime || 0;
+        var vehicleLocationsURL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=sf-muni&r=' + tag + '&t=' + epochTime;;
+
+        var p = new Promise(function(resolve, reject) {
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', vehicleLocationsURL);
+            xhr.send(null);
+            xhr.onerror = reject;
+
+            xhr.onreadystatechange = function() {
+                var DONE = 4;
+                var OK = 200;
+                if (xhr.readyState === DONE) {
+                    if (xhr.status === OK) {
+                        var parsedVehicleLocations = xmlToJson(xhr.responseXML);
+                        if (parsedVehicleLocations.hasOwnProperty('body') && parsedVehicleLocations.body.hasOwnProperty('Error')) {
+                            reject(parsedVehicleLocations.body.Error)
+                        } else {
+                            resolve(parsedVehicleLocations);
+                        }
+                    }
+                }
+            }
+        });
+
+        return p
 
     },
 
-    drawVehicles: function() {},
-    handleSubsetSelect: function() {},
+
+    drawAllRoutes: function() {
+        var _t = this;
+        _t.fetchRouteList()
+            .then(function(data) {
+                data.body.route.forEach(function(route) {
+                    var tag = route['@attributes'].tag;
+                    _t.drawVehiclesForRoute(tag);
+                })
+            })
+            .catch(function(err) {
+                console.error('Error drawing all routes', err);
+            });
+    },
+
+    drawVehiclesForRoute: function(tag) {
+        var _t = this;
+        var tag = tag || '5';
+        tag = tag.toUpperCase();
+        _t.fetchVehicleLocations(tag).then(function(locations) {
+            if (!locations.body.hasOwnProperty('vehicle')) {
+                console.log('no vehicles for route')
+                return;
+            }
+            _t.drawVehicles(locations.body.vehicle, tag)
+        }).catch(function(err) {
+            console.error('Error drawing vehicles for route', err);
+        });
+    },
+
+    drawVehicles: function(vehicles, tag) {
+        if (!vehicles) {
+            console.log('no vehicles to draw')
+            return;
+        }
+        var _t = this;
+
+        var svgGroup;
+        // if (_t.vehicleGroups.hasOwnProperty(tag)) {
+        //     d3.select('#route_' + tag).remove()
+        // }
+
+       if (_t.vehicleGroups.hasOwnProperty(tag)) {
+           svgGroup= d3.select('#route_' + tag)
+        }
+        else{
+            svgGroup = _t.svg.append("g").attr('id', "route_" + tag);
+        }
+        // var svgGroup = _t.svg.append("g").attr('id', "route_" + tag);
+        _t.vehicleGroups[tag] = svgGroup;
+
+
+        if (Array.isArray(vehicles) === false) {
+            //nextbus will return a single vehicle object instead of an array with one object if there is only one. so we make our own array
+            var temp = [];
+            temp.push(vehicles);
+            vehicles = temp;
+        }
+
+       var vehicleDots= svgGroup.selectAll("circle").data(vehicles)
+            
+            vehicleDots.exit().remove()
+
+            vehicleDots.enter()
+            .append("circle")
+            .attr("transform", function(d) {
+                return "translate(" + _t.projection([
+                    d['@attributes'].lon,
+                    d['@attributes'].lat
+                ]) + ")";
+            })
+            .attr("r", "6")
+            .attr("fill", getRandomHexColor())
+            .transition().attr("r", "10").duration(500)
+            .transition().attr("r", "6").duration(1000)
+
+            vehicleDots.attr("transform", function(d) {
+                return "translate(" + _t.projection([
+                    d['@attributes'].lon,
+                    d['@attributes'].lat
+                ]) + ")";
+            })
+
+    },
+
+    updateDrawnVehicles: function() {
+
+
+    }
+
+
 }
 
 
 var liveMapper = new te_Mapper();
-
-function Vehicle() {}
 
 function getRandomHexColor() {
     return '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
---
+++ README.md
@@ -0,0 +1,11 @@
+# To-Do
+
+-[x] base maps
+-[x] get and parse muni data
+-[x] draw and update muni vehicles
+-[] allow selection of subset of vehicles for display from HTML5 control
+-[] transitions for vehicle updates
+-[] hover data for vehicles
+-[] map zoom and pan
+-[] use an elevation service to do a sweet altitude vis
+https://developers.google.com/maps/documentation/elevation/start 
