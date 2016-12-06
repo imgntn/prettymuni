b71365309b109bdf6e12981f0e0b14517f441fd8
James B. Pollack <jamesbradenpollack@gmail.com>
2016-12-06 00:42:49 -0800

    night

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var request = require('request');
+var sslRedirect = require('heroku-ssl-redirect');
+var compression = require('compression');
 
 var app = express();
-
+app.use(compression());
+app.use(sslRedirect());
 app.use(express.static('public'))
 
 //so because nextbus is http only, we have to proxy this request to avoid mixed content warnings and still serve our app over https.
@@ -21,3 +24,11 @@ app.use('/proxy', function(req, res) {
 });
 
 app.listen(process.env.PORT || 3000);
+
+//FORCE SSL
+// app.use(function(req, res, next) {
+//   if(req.headers['x-forwarded-proto']==='http') {
+//     return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//   }
+//   next();
+// });
